8b2411f90557ff823807a9276f1db730d64df1fc
Kuddleman <edl1155@gmail.com>
2021-05-18 09:36:19 -0700

    complete Reservation class

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.Date;
+
 public class Reservation {
+    private Customer customer;
+    private IRoom room;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer: " + customer + "\n" +
+                "Room: " + room + "\n" +
+                "CheckInDate: " + checkInDate + "\n" +
+                "CheckOutDate: " + checkOutDate;
+    }
+
 }
